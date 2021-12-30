29868cfde9b06e673b3fb6f4b2bb3136b9936be1
ayaan-sayyed <84319719+ayaan-sayyed@users.noreply.github.com>
2021-12-30 21:01:36 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -17,7 +17,7 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 user_name = localStorage.getItem("user_name");
-document.getElementById("user_name").innerHTML= "Welcome" + user_name + "!";
+document.getElementById("user_name").innerHTML= " Welcome " + user_name + "!";
 
 function addRoom() {
 room_name= document.getElementById("room_name").value;
